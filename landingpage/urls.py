33ceca03c09aa6abf38bd86ff178af338e0bd1c9
mohammadmahdisheikhi <97670984+mohammadmahdisheikhi@users.noreply.github.com>
2024-07-30 21:53:42 +0330

    initial commit

---
+++ landingpage/urls.py
@@ -0,0 +1,7 @@
+# landingpage/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),  # Main landing page URL
+]
---
+++ landingpage/views.py
@@ -0,0 +1,7 @@
+# myapp/views.py
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, "landingpage/index.html")
